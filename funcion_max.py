dd33eceff4bf7ffed522d3de5a4ec3c52c741cd7
alexa665 <a.23308051281205@cbtis128.edu.mx>
2024-11-22 18:59:55 -0600

    Add files via upload

---
+++ funcion_max.py
@@ -0,0 +1,11 @@
+print(" ")
+print("Alexa Guadalupe Ramirez Manzo 3-W 1205")
+print(" ")
+def max(a, b): #funcion max
+    if a > b:  
+        return a   #funcion if y else 
+    else:          #para saber cual de los numeros es mayor al otro 
+        return b
+
+print(max(40, 45)) #num utilizados para imprimir y saber cual es mayor  
+print(" ")
